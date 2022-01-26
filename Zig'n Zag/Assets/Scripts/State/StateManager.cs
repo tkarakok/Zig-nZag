871e059caaa9c1464464450ef94d82fdbf61355c
Tunahan <59062912+tkarakok@users.noreply.github.com>
2022-01-26 13:55:54 +0300

    Event and State manager created

---
+++ Zig'n Zag/Assets/Scripts/State/StateManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum State
+{
+    MainMenu,
+    InGame,
+    Shop,
+    GameOver,
+    EndGame
+}
+
+public class StateManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
